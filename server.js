7ab18795dbc425421e829bf92406712a75635078
Jared Ruiz <jared707@gmail.com>
2022-04-23 18:31:02 -0500

    Added CREATE, DELETE, GET commands for database.

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //mysql username
+        user: 'root',
+
+        //mysql password
+        password: '',
+        database: 'election'
+    },
+    console.log('connected to the election database.')
+);
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+//DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+//CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result)
+// })
 
 //default response for any other request (not found)
 app.use((req, res) => {
